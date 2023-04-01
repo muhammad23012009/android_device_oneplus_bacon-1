4453373dc96d81e2b9f8bebb7266ab1ba78120df
Muhammad <muhammad23012009@hotmail.com>
2023-04-01 20:35:40 +0500

    treewide: Fix installer support * Rename /system mount to /system_root (needed for some odd reason) * Add patch for bootable/recovery to work with data partition install * Add fake cache service
    Signed-off-by: Muhammad <muhammad23012009@hotmail.com>

--- ubuntu/recovery/ubports-mounter.sh
+++ ubuntu/recovery/ubports-mounter.sh
@@ -2,19 +2,28 @@
 
 DATA_MOUNT_CODE=1
 
+RETRY_COUNTER=0
 while [ "$DATA_MOUNT_CODE" != "0" ]; do
-    mount -t ext4 /dev/block/by-name/userdata /data > /dev/kmsg
+    if [ "$RETRY_COUNTER" -gt "3" ]; then
+        echo "setup_fake_cache.sh - reached maximum number of retries" > /dev/kmsg
+
+        setprop halium.datamount.done 1
+        exit 1
+    fi
+    RETRY_COUNTER=$((RETRY_COUNTER+1))
+
+    mount /dev/block/by-name/userdata /data > /dev/kmsg
     DATA_MOUNT_CODE=$?
-    sleep 1
 done
 
-mkdir /cache > /dev/kmsg
-mkdir /data/android-data > /dev/kmsg
-mkdir /data/android-data/cache > /dev/kmsg
-mkdir /data/android-data/cache/recovery > /dev/kmsg
-mount -o bind /data/android-data/cache /cache > /dev/kmsgs
+mkdir /data/cache/ > /dev/kmsg
+mkdir /data/cache/recovery/ > /dev/kmsg
+
+while [ "$DATA_MOUNT_CODE" == "0" ]; do
+    if [ ! -d /cache/recovery ]; then
+        mount --bind /data/cache /cache > /dev/kmsg
+    fi
+done
 
 setprop halium.datamount.done 1
-
 exit 0
-
